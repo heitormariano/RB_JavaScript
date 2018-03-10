8b5dd7a28fb4f2d70ced1eb67658d5b0cd1cac8f
Heitor Mariano <heitor.eumesmo@gmail.com>
2018-03-10 00:55:13 -0300

    lesson 01 - improved code

--- introducao/variaveis.js
+++ introducao/variaveis.js
@@ -1,5 +1,16 @@
 var nome = 'Heitor';
 var top10 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var $scope = {title: "JavaScript", version: "6"};
+var _person = "john";
+var numberCode = 897987987;
+var a = 10;
+var language;
 
+document.getElementById("nome").innerHTML = nome;
 document.getElementById("top10").innerHTML = top10;
+document.getElementById("scope").innerHTML = $scope;
+document.getElementById("person").innerHTML = _person;
+document.getElementById("numberCode").innerHTML = numberCode;
+document.getElementById("a").innerHTML = a;
+document.getElementById("language").innerHTML = language;
 
