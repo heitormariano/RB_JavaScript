f9233c337dccd2294fac2779d421205319d92a93
Heitor Mariano <heitor.eumesmo@gmail.com>
2018-04-06 22:24:13 -0300

    lessons about strings and boolena

---
+++ boolean/boolean.js
@@ -0,0 +1,40 @@
+//usando a dupla negação (!!) para revelar o tipo booleano de certos valores
+
+
+console.log(!!0);
+console.log(!!NaN);
+console.log(!!'');
+console.log(!!false);
+console.log(!!null);
+console.log(!!undefined);
+
+var estudante = true;
+if (estudante) {
+	console.log('É estudante!');
+}else{
+	console.log('Não é estudante');
+}
+
+var nota = 10;
+
+if(nota){
+	console.log('Existe uma nota');
+}else{
+	console.log('Não existe uma nota');
+}
+
+var nome = 'Heitor';
+
+if (nome){
+	console.log('Existe um nome!');
+}else{
+	console.log('Não existe um nome');
+}
+
+//trabalhando um pouco com obetos
+var objTeste = {};
+console.log(!!objTeste);
+
+//vendo um pouco de arrays
+var arrayShow = [];
+console.log(!!arrayShow);
---
+++ strings/strings.js
@@ -0,0 +1,30 @@
+var nome = "Heitor";
+
+console.log('Caracter na posição 2 da string: ' + nome.charAt(2));
+console.log('Código do caracter que está na posição 0: ' + nome.charCodeAt(0));
+console.log('Concatenando Strings: ' + nome.concat(' Mariano'));
+console.log('Índice da primeira ocorrêcia do caracter "e": ' + nome.indexOf('e'));
+console.log('Retorando o tamanho da String: ' + nome.length);
+
+nome = 'Heitor Teste';
+console.log('Realizando a substituição de Strings: ' + nome.replace('Teste', 'Mariano'));
+
+var materias = 'Portugues;Matematica;Ingles;Fisica;Quimica';
+console.log('Dividindo a Strng: ' + materias.split(';'));
+
+var frase = 'Frase de teste para aprender JS';
+console.log(frase.substring(0, 5));
+console.log('Informando apenas o índice inicial para a substing: ' + frase.substring(9));
+
+var palavra = 'JavaScript';
+console.log('Nova String com letras maiúsculas: ' + palavra.toUpperCase());
+
+palavra = palavra.toUpperCase();
+console.log('Nova String com letras minúsculas: ' + palavra.toLowerCase());
+
+var novaFrase = '      Teste com o método trim()        ';
+console.log('Eliminando espaços em branco no início e no fim da String: ' + novaFrase.trim());
+
+novaFrase = novaFrase.trim();
+console.log('Uso do método match():');
+console.log(novaFrase.match('método'));
