b9b25d4e525d95f28962521e08727e40eda763a5
Heitor Mariano <heitor.eumesmo@gmail.com>
2018-04-22 14:54:20 -0300

    Code organization

--- introducao/variaveis.js
+++ introducao/variaveis.js
@@ -1,6 +1,9 @@
 var nome = 'Heitor';
 var top10 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-var $scope = {title: "JavaScript", version: "6"};
+var $scope = {
+  title: "JavaScript",
+  version: "6"
+};
 var _person = "john";
 var numberCode = 897987987;
 var a = 10;
@@ -13,4 +16,3 @@ document.getElementById("person").innerHTML = _person;
 document.getElementById("numberCode").innerHTML = numberCode;
 document.getElementById("a").innerHTML = a;
 document.getElementById("language").innerHTML = language;
-
